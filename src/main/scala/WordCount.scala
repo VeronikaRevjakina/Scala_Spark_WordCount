f09d688367a176fd86cda17396e0b383ec7c4174
Veronika_Revyakina <veronika.revyakina@Kronos.com>
2020-05-17 04:42:28 +0300

    fixed regex, added stop words

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -12,14 +12,17 @@ object WordCount {
     sc.setLogLevel("ERROR")
     // Load our input data.
     val input =  sc.textFile("src/main/resources/book.txt")
+    val stopWords =  sc.textFile("src/main/resources/stop_words.txt").collect().toSet
     // Split up into words.
-    val words = input.flatMap(line => line.split(" ")).map(word => word.toLowerCase)
+    val words = input.flatMap(line => line.split("\\W+")).map(word => word.toLowerCase)
     // Transform into word and count.
-    val counts = words.map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2,false)
+    val wordsFiltered = words.filter(!stopWords.contains(_))
+    val counts = wordsFiltered.map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2,false)
     // Save the word count back to csv file and cut TOP =100.
 //    counts.saveAsTextFile("src/main/resources/result")
     val spark = SparkSession.builder.config(conf).getOrCreate()
     val df = spark.createDataFrame(counts).toDF("word", "amount")
+
     val result = df.limit(100)
 
     result.write.format("csv").save("src/main/resources/result")
